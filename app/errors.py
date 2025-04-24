641f00bacd804d706943fd7cf692e59a9508a0a3
Vitalik <lg.mobile0808@gmail.com>
2025-04-24 22:50:23 +0300

    Solution

--- app/errors.py
+++ app/errors.py
@@ -1,14 +1,18 @@
 class VaccineError(Exception):
+    """Base class for vaccine-related errors."""
     pass
 
 
-class NotVaccinatedError(Exception):
+class NotVaccinatedError(VaccineError):
+    """Raised when a person is not vaccinated."""
     pass
 
 
-class OutdatedVaccineError(Exception):
+class OutdatedVaccineError(VaccineError):
+    """Raised when a person has an expired vaccine."""
     pass
 
 
 class NotWearingMaskError(Exception):
+    """Raised when a person is not wearing a mask."""
     pass
